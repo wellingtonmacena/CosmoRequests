8c878cf5d24fb90d40c5189e7117756c085158cf
wellingtonmacena <wellington.macena.23@gmail.com>
2020-12-27 23:49:13 -0300

    Added GET and POST methods into project

---
+++ CosmoRequests/CosmoRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Helper;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmoRequests
+{
+    public class CosmoRequest
+    {
+        WebRequest webRequest;
+
+        public Response GET(string url)
+        {
+            this.webRequest = WebRequest.Create(url);
+            this.webRequest.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+
+            Response response1 = new Response(response, Helper<int>.GetString(response));
+
+            return response1;
+        }
+    }
+}
+
+
